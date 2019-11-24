2834b167c460beb3138b35c7fad459703507f573
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-24 13:49:57 +0300

    register database to settings

--- awards/urls.py
+++ awards/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('rateme.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    # url(r'^accounts/', include('django.contrib.auth.urls')),
-    # url(r'^logout/$', views.logout, {'next_page': '/'}),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', views.logout, {'next_page': '/'}),
 ]
--- rateme/models.py
+++ rateme/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -32,4 +32,4 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.project.
+        return self.project

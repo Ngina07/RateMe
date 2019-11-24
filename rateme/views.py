30dc82eabe997bde43d8dbe444f792072c3ac253
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-24 19:32:06 +0300

    create view function to display home page with projects

--- rateme/views.py
+++ rateme/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import Profile,Project,Review
 
 
 
 
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    if User.objects.filter(username = request.user.username).exists():
+        user = User.objects.get(username=request.user)
+        if not Profile.objects.filter(user = request.user).exists():
+            Profile.objects.create(user = user)   
+    projects = Project.objects.order_by('-pub_date')
+
+    return render(request,"welcome.html",{"projects":projects})
 
 

257b81dcfa7b290d9511992992760b8a8b0929d8
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-24 19:54:56 +0300

    create review form for users to rate projects

--- rateme/forms.py
+++ rateme/forms.py
@@ -6,3 +6,9 @@ class UploadProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['user','pub_date']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ['average','project', 'user']
+
--- rateme/views.py
+++ rateme/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Profile,Project,Review
-from .forms import UploadProjectForm
-
+from .forms import UploadProjectForm,ReviewForm,UpdateProfile
+from django.db.models import Avg
 
 
 

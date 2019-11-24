7106327a4fbe30f83f68e7841f31ed2c861b9f4c
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-24 13:34:30 +0300

    add Profile models to allow user to save their biodata

--- rateme/models.py
+++ rateme/models.py
@@ -13,3 +13,12 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='images/', default='/images/default.jpg')
+    bio = models.TextField(blank=True)
+    contacts = models.CharField(max_length = 30,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
